139f88446788afad22649d5fe84dbcd8b2cfafbb
maks_kras <maksym_krasovskiy@ya.ru>
2025-09-08 20:58:33 +0300

    fixes func masks.py

--- src/masks.py
+++ src/masks.py
@@ -2,7 +2,7 @@ def get_mask_card_number(card_number: str) -> str:
     """Функция маскирует номер банковской карты"""
     card_number = card_number.replace(" ", "")
     if card_number.isdigit() and len(card_number) == 16:
-        return f"{card_number[:4]} {card_number[6:8]}** **** {card_number[-4::]}"
+        return f"{card_number[:4]} {card_number[4:6]}** **** {card_number[-4:]}"
     return "Вы ввели не существующий номер карты."
 
 
@@ -10,7 +10,7 @@ def get_mask_account(account_number: str) -> str:
     """Функция маскирует банковский счёт"""
     account_number = account_number.replace(" ", "")
     if account_number.isdigit() and len(account_number) == 20:
-        return f"**{account_number[-4::]}"
+        return f"**{account_number[-4:]}"
     return "Вы ввели не существующий номер счёта."
 
 
