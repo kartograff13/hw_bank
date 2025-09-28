9c43be0c0e4995996aa29fce473e551aa54de745
maks_kras <maksym_krasovskiy@ya.ru>
2025-09-29 00:06:38 +0300

    correct test for widget.py

--- tests/test_widget.py
+++ tests/test_widget.py
@@ -7,6 +7,7 @@ from src.widget import get_date, mask_account_card
     "account_info, expected",
     [
         ("Maestro 1596837868705199", "Maestro 1596 83** **** 5199"),
+        ("Mastro 1596837868705199", "Вы ввели не существующий номер карты либо счёта."),
         ("Maestro 15968378687051991", "Вы ввели не существующий номер карты либо счёта."),
         ("MasterCard 7158300734726758", "MasterCard 7158 30** **** 6758"),
         ("Visa Classic 6831982476737658", "Visa Classic 6831 98** **** 7658"),
