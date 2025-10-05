094790e96cc2becfee66d4199074ac9c48bcf7ef
maks_kras <maksym_krasovskiy@ya.ru>
2025-10-05 16:40:12 +0300

    init test_generators.py and add funcs tests'

--- src/generators.py
+++ src/generators.py
@@ -18,4 +18,4 @@ def card_number_generator(start: int, end: int) -> Iterator[str]:
     """Генератор, который выдает номера банковских карт в формате XXXX XXXX XXXX XXXX в заданном диапазоне"""
     for number in range(start, end + 1):
         formatted_number = f"{number:016d}"
-        yield " ".join([formatted_number[i : i + 4] for i in range(0, 16, 4)])
+        yield " ".join([formatted_number[i: i + 4] for i in range(0, 16, 4)])
