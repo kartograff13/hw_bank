daaeec017a1286dfbe9262f5aa2d24749f1a48ed
maks_kras <maksym_krasovskiy@ya.ru>
2025-10-05 15:37:23 +0300

    add func card_number_generator in module generators.py

--- src/generators.py
+++ src/generators.py
@@ -9,5 +9,13 @@ def filter_by_currency(transactions: list[dict], currency: str) -> Iterator[dict
 
 
 def transaction_descriptions(transactions: list[dict]) -> Iterator[dict]:
+    """Генератор, который возвращает описание каждой операции"""
     for transaction in transactions:
         yield transaction["description"]
+
+
+def card_number_generator(start: int, end: int) -> Iterator[str]:
+    """Генератор, который выдает номера банковских карт в формате XXXX XXXX XXXX XXXX в заданном диапазоне"""
+    for number in range(start, end + 1):
+        formatted_number = f"{number:016d}"
+        yield " ".join([formatted_number[i : i + 4] for i in range(0, 16, 4)])
