4045609f6e9798a63f5e48955ca25a8b8ec00297
maks_kras <maksym_krasovskiy@ya.ru>
2025-10-22 09:33:50 +0300

    correct utils.py

--- src/utils.py
+++ src/utils.py
@@ -12,7 +12,7 @@ log_file = os.path.join(log_dir, "transactions_logger.log")
 logging.basicConfig(
     level=logging.INFO,
     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
-    datefmt="%d-%m-%Y %H:%M:%S",  # Добавлен формат даты и времени
+    datefmt="%d-%m-%Y %H:%M:%S",
     handlers=[
         logging.FileHandler(log_file, mode="w", encoding="utf-8"),
     ],
